e896c467df065f7fd3de16fdaeac2259763808cd
terenyijanos96 <terenyi.janos96@gmail.com>
2024-02-13 15:14:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import { useState } from "react";
-import "./App.css";
-import Gomb from "./components/Gomb";
-import Kep from "./components/Kep";
+import './App.css';
+import Gomb from './components/Gomb';
+
 
 function App() {
-  const kepek = [
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Mudi_portrait.jpg/260px-Mudi_portrait.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Stbernardinsnow.jpg/1024px-Stbernardinsnow.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/4/41/Siberian_Huskies_line.jpg",
-  ];
 
-  const [kepindex, setKepIndex] = useState(0);
+  let kattintasSzam = 0
 
-  function valtoztat(id) {
-    if (id === "gomb-hatra") {
-      console.log("Hátra...");
-      if (kepindex === 0) {
-        setKepIndex(kepek.length - 1);
-      } else {
-        setKepIndex(kepindex - 1);
-      }
-    }
-
-    if (id === "gomb-elore") {
-      console.log("Előre...");
-      if (kepindex === kepek.length - 1) {
-        setKepIndex(0);
-      } else {
-        setKepIndex(kepindex + 1);
-      }
-    }
+  function kiiratas(id){
+    kattintasSzam++
+    console.log(kattintasSzam)
+    console.log(id)
   }
 
   return (
     <div className="App">
-      <Gomb fuggveny={valtoztat} szoveg="hátra" id="gomb-hatra" />
-      <Kep kep={kepek[kepindex]} />
-      <Gomb fuggveny={valtoztat} szoveg="előre" id="gomb-elore" />
+        <Gomb fuggveny={kiiratas} id="gomb-1"/>
+        <Gomb fuggveny={kiiratas} id="gomb-2"/>
+        <Gomb fuggveny={kiiratas} id="gomb-3"/>
     </div>
   );
 }
--- src/components/Gomb.js
+++ src/components/Gomb.js
@@ -1,12 +1,19 @@
+import { useState } from "react"
 
 export default function Gomb(props){
+    const [szam, setSzam] = useState(0);
 
+    // const lista = useState(0)
+    // const szam = lista[0]
+    // const setSzam = lista[1]
 
-    function atad(){
+    
+    function novel(){
+        setSzam(szam + 1)
         props.fuggveny(props.id)
     }
 
     return(
-        <button onClick={atad}>{props.szoveg}</button>
+        <button onClick={novel}>Kattintás: {szam}</button>
     )
 }
